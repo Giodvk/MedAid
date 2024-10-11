6be5ff5db18e41b97deb99fb62af3cf892f9283d
Giodvk <giovannicasaburi02@gmail.com>
2024-10-11 14:42:32 +0200

    settimo commit cumulativo

---
+++ Assets/Free Wood Door Pack/Script/CameraOpenDoor.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CameraDoorScript
+{
+public class CameraOpenDoor : MonoBehaviour {
+	public float DistanceOpen=3;
+	public GameObject text;
+	private TextMeshProUGUI interazione;
+	// Use this for initialization
+	void Start () {
+		if(text)
+		interazione=text.GetComponent<TextMeshProUGUI>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit hit;
+		if (Physics.Raycast (transform.position, transform.forward, out hit, DistanceOpen) && interazione) {
+				if (hit.transform.GetComponent<DoorScript.Door> ()) {
+				interazione.enabled = true;
+				if (Input.GetKeyDown(KeyCode.E)){
+					hit.transform.GetComponent<DoorScript.Door> ().OpenDoor();
+				if(interazione.text=="Aprire"){
+					interazione.text="Chiudere";
+				}else{
+					interazione.text="Aprire";
+				}
+				}
+			}else{
+				interazione.enabled = false;
+			}
+		}else if(interazione){
+			interazione.enabled = false;
+		}
+	}
+}
+}
